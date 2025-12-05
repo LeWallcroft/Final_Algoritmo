a3f6606ba8de9bfcf849eef1788cc047412bb580
Erick <diegoenriquez942@gmail.com>
2025-12-04 22:52:27 -0500

    Inicializacion de implementacion

---
+++ EF.py
@@ -0,0 +1,13 @@
+import time
+
+# ====================================================
+# FUNCIONES AUXILIARES
+# ====================================================
+
+def Minimo3(a, b, c):
+    return min(a, b, c)
+
+
+def MinimoSimple(a, b):
+    return min(a, b)
+
